eebdf008cbf65f276c7fb2a6bd1e5b3329763fae
RomanCLV <roman.clavier.2001@gmail.com>
2021-03-29 14:11:41 +0200

    init project

---
+++ main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Main module
+
+Copyright © 2021 Roman Clavier
+
+Python describing
+"""
+
+
+def main():
+    """
+    The main function
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
