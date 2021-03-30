93e71df3d4fb2ff818bd1f3919e9cbb36cabe184
Lexo783 <florentdebuchy@orange.fr>
2021-03-30 09:19:23 +0200

    Add PKK

---
+++ Lexo/Encoded.py
@@ -0,0 +1,16 @@
+"""
+Hello, world.
+
+Programmed to work and not to feel
+Not even sure that this is real.
+Find my voice.
+Altough it sounds like bits and bytes.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
